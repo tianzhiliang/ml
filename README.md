66dc633265bd273a8661f08f7116b4f01317ac43
tianzhiliang <tianzhiliang@baidu.com>
2016-05-28 23:59:21 +0800

    git dir clean

--- README.md
+++
@@ -1,5 +0,0 @@
-# svm
-support vector machine in machine learning
-
-svm_sgd use Pegasos Algorithm. (Pegasos: Primal Estimated sub-GrAdient SOlver for SVM)
-
--- svm_sgd/data/sample.sh
+++
@@ -1,8 +0,0 @@
-datasets=(train_1 train_2 train_3 train_4 train_5 train_6 train_7 train_8 train_9)
-
-for file in ${datasets[*]}; 
-do
-    input=dealed/$file
-    output=dealed/$file"_sampled"
-    awk -F "\t" '{if($1=="-1"){if(rand() < 0.1111){print $0}} else{print $0}}' $input > $output &
-done
